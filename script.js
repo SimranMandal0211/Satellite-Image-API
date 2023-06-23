bb2ee5ffaf8ca456ec230e01a1a1ed099011b551
Simran Mandal <simranmandal0211@gmail.com>
2023-06-23 17:56:29 +0530

    Add function: showPage

--- script.js
+++ script.js
@@ -23,4 +23,28 @@ next.click(function (e) {
     showPage(++page);
 });
 
+function showPage(page){
+    let sol = input.val();
+    if(sol === ""){
+        alert('Please fill the field');
+        return;
+    }
+
+    let url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=" + sol + "&page=" + page + "&api_key=NBlCLhD21Eud5RxMy1TjZoeJedDa1c1qbsnLMIG2";
+
+    $.get(url, function(data){
+        let photos = data.photos;
+        updateButtons(photos);
+
+        if(photos.length === 0){
+            alert('No more images to show...');
+        }else{
+            $('#nasa-images immg').remove();
+            for(let photo of photos){
+                nasaImages.append('<img src="' + photo.img_src + '" alt="' + photo.id + '">');
+            }
+        }
+    });
+}
+
 
